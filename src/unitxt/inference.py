a3989174a60c5f2d228cde7d760b4fd890d76bd9
ChanderG <mail@chandergovind.org>
2025-10-27 15:05:13 +0530

    use native models
    some models (Granite 4 MoE in particular) do not seem to work with HF version

--- src/unitxt/inference.py
+++ src/unitxt/inference.py
@@ -3054,7 +3054,6 @@ class VLLMInferenceEngine(InferenceEngine, PackageRequirementsMixin, VLLMParamsM
             max_num_seqs=64,
             enforce_eager=True,
             dtype="bfloat16",
-            model_impl="transformers",
         )
 
     def _infer(
